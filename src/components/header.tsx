a6430a475a57a03f9c748b68c546fa781597f191
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-03-01 22:29:57 -0500

    Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="flex items-center justify-between mx-auto p-8">
+      <Link href="/" className="font-bold text-3xl">Home</Link>
+
+      <div className="space-x-4 text-xl">
+        <Link href="/members">Members</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </nav>
+  );
+}
