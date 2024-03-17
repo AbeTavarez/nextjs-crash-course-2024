2df099f68b93b08214e3fece0a68d91eedcf8bfc
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-03-17 12:45:41 -0400

    Fetching Member by id [id]

--- src/app/members/[id]/page.tsx
+++ src/app/members/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { members } from "@/db";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import dbClient from "@/db/mongodb";
+import { ObjectId } from "mongodb";
 
 interface MemberPageProps {
   params: {
@@ -8,8 +10,16 @@ interface MemberPageProps {
   };
 }
 
-export default function MemberPage(props: MemberPageProps) {
-  const member = members.find((m) => m.id === props.params.id);
+export default async function MemberPage(props: MemberPageProps) {
+  // const member = members.find((m) => m.id === props.params.id);
+
+  const member = await dbClient
+    .db("next-members")
+    .collection("members")
+    .findOne({ _id: new ObjectId(props.params.id) });
+
+    console.log(member);
+    
 
   if (!member) {
     return notFound();
@@ -17,21 +27,19 @@ export default function MemberPage(props: MemberPageProps) {
 
   return (
     <div className="container mx-auto bg-blue-700">
-      
       <div className="p-2 flex items-center">
         <Image
-        width={200}
-        height={200}
-        src={member.avatar} alt="member photo" 
-        className="border-4 rounded-full"
+          width={200}
+          height={200}
+          src={member.avatar}
+          alt="member photo"
+          className="border-4 rounded-full"
         />
 
         <div className="flex-col text-white ml-5">
           <div className="text-2xl font-bold">{member.name}</div>
           <div>Member Since: {member.createdAt}</div>
         </div>
-
-
       </div>
     </div>
   );
